a969223aa6b20ff0d5732c2d9f3c133b72168a2f
Joon <joonqpa@gamil.com>
2023-01-13 19:08:30 +0900

    api기능 체크

--- api.js
+++ api.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const app = express();
+const uuidAPIKey = require('uuid-apikey');
 
 const server = app.listen(3000, () => {
     console.log('start server : localhost:3000');
 });
 
-app.get('/api/users/:type', async (req, res) => {
-    let {type} = req.params;
+
+const key = {
+    apiKey: 'J3VMYCC-N9K47ZG-PXR7HA0-AH94H32',
+    uuid: '90f74f31-aa66-43fe-b770-78a85452488c'
+}
+
+app.get('/api/users/:apikey/:type', async (req, res) => {
+    let {
+        apikey,
+        type
+    } = req.params;
+
+    if(!uuidAPIKey.isAPIKey(apikey) || !uuidAPIKey.check(apikey, key.uuid)){
+        res.send('apikey is not valid.')
+    }else{
+
+    }
 
     if(type == 'seoul') {
         let data = [
